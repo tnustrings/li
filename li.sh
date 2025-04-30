7ab722aaf3a2fd70f9e5ff032dfdc945ad501d10
numlims <max.haubold@med.uni-greifswald.de>
2025-04-30 10:10:46 +0200

    ct fix

--- li.sh
+++ li.sh
@@ -1,41 +1,36 @@
 li() {
-    
-    is a dir given, we run li for the given dir, else we run it for the
-    current dir.
-    
-    go through the files, take the directories and see if there's an
-    index.txt / readme in them.
-    
-    we'd like to include a range of readme capitalization patterns, but
-    there is no case-insensitive ls. listing all files and grep -i
-    "readme" makes it slow. so maybe ls and take the first (but is that
-    really quicker?)
-    
-    redirect 'file not found' stderrs to /dev/null.
-    
-    ls returns the files sorted alphabetically.
-    
-    is the index/readme is there, print the directory name without path
-    (basename) and followed by the first non-heading line from the
-    index/readme (the first line starting with a word character).
-    
-    ``./there:
-    
-    		basename=$(basename $f)
-    		echo -n "$basename: "
-    		egrep "^\w" --max-count 1 $file
-    ``                
-    
-    if the index/readme is not there, and the -a flag is set, print the directory.
-    
-    ``../not there:
-    		if [[ $a_flag == "true" ]]; then
-    		    echo $f
-    		fi
+    a_flag=''
+    while getopts 'a' flag; do # for non-boolean flags follow flag letter by colon
+	case "${flag}" in
+	    a) a_flag='true' ;;
+	esac
+    done
+    if [[ $# > 0 ]]; then
+	# if not a directory, return? 
+	if [[ ! -d $1 ]]; then
+	    return
+	fi
+	where=$1/*
+    else
+	where=*
+    fi
+    for f in $where; do
+	# is f a directory?
+	if [[ -d $f ]]; then
+            	    file=$(ls -1 ${f}/index.txt ${f}/readme.* ${f}/Readme.* ${f}/README.* ${f}/readme ${f}/Readme ${f}/README -R 2>/dev/null | head -1) # ls -1: one by line
+                  	    if [[ -f $file ]]; then
+                            
+                            		basename=$(basename $f)
+                            		echo -n "$basename: "
+                            		egrep "^\w" --max-count 1 $file
+                        else
+                            		if [[ $a_flag == "true" ]]; then
+                            		    echo $f
+                            		fi
+                        fi
+        fi
+    done
 }
-
-look for a -a (all) flag with getopts
-see https://stackoverflow.com/a/21128172
 ci() {
     # cd into the given directory
     cd "$1"
